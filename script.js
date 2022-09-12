5e3fcdd04e547f30d8c2fec77841ad349098b6d8
Arsenica Sebastian <sarsenica537@gmail.com>
2022-09-12 22:25:27 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.querySelector('.menu').addEventListener('click',()=>{ 
+    document.querySelectorAll(".target").forEach((item)=>{ 
+    item.classList.toggle('change')
+    })
+})
+
+
+let icons = document.querySelectorAll('.section-1-icons i')
+let i = 1
+
+setInterval(()=>{ 
+    i++
+let icon = document.querySelector('.section-1-icons .change')
+
+icon.classList.remove('change')
+
+if(i> icons.length){
+    i=1; 
+    icons[0].classList.add('change')
+}
+else
+icon.nextElementSibling.classList.add('change')
+},4000)
